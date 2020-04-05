6a1a46d448762802d8e45b35d31abf665f22a0d6
harlanhappydog <harlancampbell@gmail.com>
2020-04-05 16:55:37 -0700

    Update MEMA_simple.R

--- MEMA_simple.R
+++ MEMA_simple.R
@@ -920,3 +920,122 @@ model {
   	}"
 
 
+
+
+#######
+
+
+
+#### BAYES MEMA4 (with no bounds for PHI_):
+BayesMEMA4 <- 
+"data {
+  int<lower=0> NStudies;		
+  vector<lower=0>[NStudies]  n_per_study;
+  vector[NStudies] alpha_hat_star;
+  vector[NStudies] beta_hat_star;
+  vector[NStudies] mu_hat_star;
+  vector<lower=0>[NStudies] sigma_hat_star;
+  vector<lower=0>[NStudies] lambda_hat_star;
+}
+
+transformed data {
+  matrix[NStudies,2] coef_vec_hat;
+
+  for (k in 1:NStudies)
+    coef_vec_hat[k,1] = alpha_hat_star[k];	
+  for (k in 1:NStudies)
+    coef_vec_hat[k,2] = beta_hat_star[k];	
+}
+
+
+
+parameters {
+  vector<lower=0>[NStudies] PHI_;		
+  vector[NStudies] alpha;
+  vector[NStudies] beta;
+  real theta;
+  real<lower=0> TAU_;
+  real XI_;
+  real<lower=0> OMEGA_;
+}
+
+transformed parameters {
+  vector[NStudies]    gamma;	
+  vector[NStudies]    mu_hat;
+  vector[NStudies]    sigma_hat;
+  vector[NStudies]    lambda_hat;
+  vector[NStudies]    var_alphahat_star;
+  vector[NStudies]    var_betahat_star;	
+  matrix[2, 2]        SIG_star[NStudies];  
+  vector[NStudies]    alpha_star;
+  vector[NStudies]    beta_star;
+  matrix[NStudies, 2] alphabeta_star;	
+  vector<lower=0>[NStudies] PHI_squared;  
+  
+  for (k in 1:NStudies)
+	PHI_squared[k] = PHI_[k]^2;
+
+  for (k in 1:NStudies)
+  	gamma[k] = ( 1+ PHI_squared[k]/((lambda_hat_star[k]^2) - PHI_squared[k]))^(-1);
+
+  for (k in 1:NStudies)  
+	mu_hat[k] = mu_hat_star[k];
+  
+   for (k in 1:NStudies)  
+     lambda_hat[k] = sqrt(lambda_hat_star[k]^2 - PHI_squared[k]);
+  
+  for (k in 1:NStudies)  
+	sigma_hat[k] = sqrt((sigma_hat_star[k])^2 - (1 - gamma[k])*(beta[k]^2)*(lambda_hat[k]^2))  ;
+  
+  for (k in 1:NStudies)
+	var_alphahat_star[k] = ((lambda_hat_star[k]^2 + mu_hat_star[k]^2)* sigma_hat_star[k]^2)/((lambda_hat_star[k]^2)* n_per_study[k]);	
+
+  for (k in 1:NStudies)
+	var_betahat_star[k] = (sigma_hat_star[k]^2)/((lambda_hat_star[k]^2)* n_per_study[k]);	
+
+  for (k in 1:NStudies)
+  	SIG_star[k][1,1] = var_alphahat_star[k];
+
+  for (k in 1:NStudies)
+  	SIG_star[k][2,2] = var_betahat_star[k];
+
+  for (k in 1:NStudies)
+  	SIG_star[k][1,2] = - mu_hat_star[k]*(sigma_hat_star[k]^2)/((lambda_hat_star[k]^2)* n_per_study[k]);  	
+
+  for (k in 1:NStudies)
+  	SIG_star[k][2,1] = - mu_hat_star[k]*(sigma_hat_star[k]^2)/((lambda_hat_star[k]^2)* n_per_study[k]);  	
+
+
+  for (k in 1:NStudies)
+    alpha_star[k] = alpha[k] + (1-gamma[k])*beta[k]*mu_hat[k];
+
+  for (k in 1:NStudies)
+    beta_star[k] = gamma[k]*beta[k];
+
+  alphabeta_star = append_col(alpha_star, beta_star);	
+
+
+
+}
+
+
+model {
+  
+  theta ~ normal(0, 10);  
+  TAU_ ~ cauchy(0, 2.5);
+  XI_ ~ normal(0, 10);  
+  OMEGA_ ~ cauchy(0, 2.5);
+    
+  for (k in 1:NStudies)
+	PHI_[k] ~ uniform(0, bb^2);
+
+  bb ~ uniform(0.1, 100);
+
+  for (k in 1:NStudies)	
+	coef_vec_hat[k,] ~ multi_normal(alphabeta_star[k,], SIG_star[k]);
+  for (k in 1:NStudies)
+	alpha[k] ~ normal(XI_, OMEGA_);
+  for (k in 1:NStudies)
+	beta[k] ~ normal(theta, TAU_);
+
+  	}"
